18125dcd6460c99d476c4f6f553452aacafd603c
Youssef Essam <youssef.essam.taha@gmail.com>
2021-03-26 16:24:10 +0200

    adding C folder

---
+++ Assignment/1/Printing/Printing.c
@@ -0,0 +1,16 @@
+/*
+ * Printing.c
+ *
+ *  Created on: Mar 26, 2021
+ *      Author: Hamlet
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	printf("C Programming");
+
+	return 0;
+}
